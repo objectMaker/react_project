9f094bbd715fc8c6964f54bef8efd3479906431f
objectMaker <528154320@qq.com>
2021-05-24 00:07:00 +0800

    还没有完成验证功能

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,53 @@
+import React,{Component,Fragment} from 'react'
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import './index.less'
+export default class abc extends Component {
+    render() {
+            const onFinish = (values) => {
+                console.log('Received values of form: ', values);
+            }
+            return (<Fragment>
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入你的用户名!',
+                            },
+                        ]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入你的密码!',
+                            },
+                        ]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Fragment>)
+        }
+}
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,21 @@
-import {Component} from "react";
+import {Component,Fragment} from "react";
+import Form from '../../components/Form'
+import './index.less'
 export default class Login extends Component{
     render() {
-        return (<div id="gou">
-            我是login组件
-        </div>)
+        return (<Fragment>
+            <div className="header">
+                <div className="header_left">
+                    左边图片
+                </div>
+                <div className="header_title">
+                    商品后台
+                </div>
+            </div>
+            <div className="form">
+                <div className="login">登录</div>
+                <Form/>
+            </div>
+        </Fragment>)
     }
 }
